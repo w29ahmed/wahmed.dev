56f62d1a93cab0b502a8459f87b73facab6bacf1
Waleed Ahmed <w29ahmed@uwaterloo.ca>
2020-12-04 22:24:47 -0500

    added react-bootstrap

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Container className="p-3">
+      <Jumbotron>
+        <h1 className="header">
+          Welcome To React-Bootstrap TypeScript Example
+        </h1>
+      </Jumbotron>
+
+      <h1 className="header">Hello World</h1>
+      <Button variant="primary" className="mr-1">
+        Primary
+      </Button>
+    </Container>
+  );
 }
 
 export default App;
