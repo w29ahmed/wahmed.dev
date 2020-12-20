c998eeb7abb3911e80bacbc1e8d496494359795c
Waleed Ahmed <w29ahmed@uwaterloo.ca>
2020-12-20 00:49:23 -0500

    project card skeleton

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Card from "react-bootstrap/esm/Card";
+import Badge from "react-bootstrap/Badge";
+import { FaGithub } from "react-icons/fa";
+import projects from "./content/projects.json";
 import "./styles/Projects.scss";
 
 const Projects = () => {
@@ -9,6 +15,38 @@ const Projects = () => {
         <h1>
           Projects <hr />
         </h1>
+
+        <Row className="project-row">
+          {projects.projects.map((project) => (
+            <Col lg="4" md="6">
+              <Card>
+                <Card.Header>
+                  <h4>{project.title}</h4>
+                </Card.Header>
+
+                <Card.Body>
+                  {project.skills.map((skill) => (
+                    <Badge pill variant="primary">
+                      {skill}
+                    </Badge>
+                  ))}
+
+                  <ul>
+                    {project.bullets.map((bullet) => (
+                      <li>{bullet}</li>
+                    ))}
+                  </ul>
+
+                  {project.githubLinks.map((ghLink) => (
+                    <a href={ghLink}>
+                      <FaGithub />
+                    </a>
+                  ))}
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </Container>
     </section>
   );
