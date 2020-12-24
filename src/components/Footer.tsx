d691fd0662de05d54d0e9ae6f6150fcef80f4c47
Waleed Ahmed <w29ahmed@uwaterloo.ca>
2020-12-23 22:26:19 -0500

    footer content and styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import {
+  FaRegCopyright,
+  FaEnvelope,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
+import contact from "./content/contact.json";
 import "./styles/Footer.scss";
 
 const Footer = () => {
   return (
     <footer id="contact">
-        <p>Footer</p>
+      <FaRegCopyright className="copyright" />
+      <p>{new Date().getFullYear()} Waleed Ahmed</p>
+      <a href={"mailto:" + contact.email}>
+        <FaEnvelope className="icon mail" />
+      </a>
+      <a href={contact.linkedin} target="_blank" rel="noopener noreferrer">
+        <FaLinkedin className="icon" />
+      </a>
+      <a href={contact.github} target="_blank" rel="noopener noreferrer">
+        <FaGithub className="icon" />
+      </a>
     </footer>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,25 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import Button from 'react-bootstrap/Button'
+import Button from "react-bootstrap/Button";
 import "./styles/Header.scss";
 
 const Header = () => {
   return (
-    <Container className="header" fluid>
-      <Navbar expand="md" variant="light" bg="light">
-        <Navbar.Brand href="#">Navbar</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav className="ml-auto">
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#experience">Experience</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#education">Education</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Button className="resume-button">Resume</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Container>
+    <Navbar expand="md" variant="light" bg="light" className="header">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse>
+        <Nav className="ml-auto">
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#skills">Skills</Nav.Link>
+          <Nav.Link href="#experience">Experience</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#education">Education</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Button className="resume-button">Resume</Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
