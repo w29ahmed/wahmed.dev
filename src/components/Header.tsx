6dbcc74a3ed44a42d1479d61254032b282d93ab7
Waleed Ahmed <w29ahmed@uwaterloo.ca>
2020-12-25 16:09:38 -0500

    theme toggle button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 import Link from "./Link";
+import Switch from "react-switch";
+import { FaSun, FaMoon} from "react-icons/fa";
 import resume from "./content/Waleed_Ahmed_Resume.pdf";
 import "./styles/Header.scss";
 
@@ -16,15 +18,34 @@ const sections = [
 ];
 
 const Header = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
     <Navbar
       sticky="top"
       collapseOnSelect
       expand="md"
-      variant="light"
-      bg="light"
+      variant={darkTheme ? "dark" : "light"}
+      bg={darkTheme ? "dark" : "light"}
       className="header"
     >
+      <label htmlFor="theme-switch">
+        <Switch
+          checked={darkTheme}
+          onChange={() => setDarkTheme((prev) => !prev)}
+          onColor="#814ad8"
+          handleDiameter={30}
+          uncheckedIcon={<FaSun className="sun"/>}
+          checkedIcon={<FaMoon className="moon"/>}
+          boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+          activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+          height={25}
+          width={50}
+          className="theme-switch"
+          id="theme-switch"
+        />
+      </label>
+
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse>
         <Nav className="ml-auto">
