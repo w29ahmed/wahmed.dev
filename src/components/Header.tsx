dcf9aff6ef2f3cadbd4202d9c13ef31aa1fcd2ae
Waleed Ahmed <w29ahmed@uwaterloo.ca>
2020-12-24 20:25:22 -0500

    added pdf resume

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 import Link from "./Link";
+import resume from "./content/Waleed_Ahmed_Resume.pdf";
 import "./styles/Header.scss";
 
 const sections = [
@@ -30,7 +31,9 @@ const Header = () => {
           {sections.map((section) => (
             <Link section={section} />
           ))}
-          <Button className="resume-button">Resume</Button>
+          <a href={resume} target="_blank" rel="noopener noreferrer">
+            <Button className="resume-button">Resume</Button>
+          </a>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
